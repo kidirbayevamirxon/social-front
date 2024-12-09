b4d7ec0e79354c43b9892db1867f3b26cc420462
kidirbayevamirxon <amirxonkidirbayev0@gmail.com>
2024-12-09 15:33:27 +0500

    pashty biti

---
+++ js/post.js
@@ -0,0 +1,17 @@
+const fileInput = document.getElementById('value1');
+    const displayImg = document.getElementById('displayimg');
+
+    fileInput.addEventListener('change', function () {
+        const file = this.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                displayImg.src = e.target.result;
+                displayImg.style.display = 'block';
+            };
+            reader.readAsDataURL(file);
+        } else {
+            displayImg.style.display = 'none';
+            displayImg.src = '';
+        }
+    });
