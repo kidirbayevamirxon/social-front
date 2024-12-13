b27e73c1eb2bf8b0cf10e14de73ec25b358e71a9
kidirbayevamirxon <amirxonkidirbayev0@gmail.com>
2024-12-13 16:11:51 +0500

    dxfhg

--- js/logo.js
+++ js/logo.js
@@ -1,2 +1,48 @@
 import "../css/logo.css";
 
+const logodiv=document.querySelector('.logodiv')
+const form=document.querySelector('.form')
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const ldiv=document.createElement('div')
+    logodiv.appendChild(ldiv)
+    
+    const topdiv=document.createElement('div')
+    ldiv.appendChild(topdiv)
+    
+    const img=document.createElement('img')  
+    const p1=document.createElement('p')
+    const p2=document.createElement('button')
+    topdiv.appendChild(img)
+    topdiv.appendChild(p1)
+    topdiv.appendChild(p2)
+    
+    p2.type='button'
+    ldiv.className="ldiv"
+    topdiv.className="topdiv"
+    img.className="lrimg"
+    p1.className="p1"
+    p2.className="p2"
+    
+    p2.textContent="Follow"
+
+    const username = localStorage.getItem("username");
+    const profileNameElement = document.querySelector(".p1");
+
+    if (profileNameElement && username) {
+        profileNameElement.textContent = `${username}`;
+      } else {
+        console.error("Foydalanuvchi aniqlanmadi yoki element topilmadi.");
+      }
+      p2.addEventListener('click',()=>{
+         if (p2.textContent==="Follow") {
+        p2.textContent="Unfollow"
+
+    }else{
+        p2.textContent="Follow"
+    }
+      })
+      
+   
+})
